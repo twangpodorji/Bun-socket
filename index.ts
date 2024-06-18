bf41d14764995925cdfbd145957b549acaa0e535
douglasswmcst <douglas.cst@rub.edu.bt>
2024-06-18 21:04:30 +0600

    update: string the json response and accept the auth header from the cookie

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 var jwt = require("jsonwebtoken");
+var cookie = require("cookie");
 
 const server = Bun.serve<{ userId: string; channelId: string }>({
+  port: 4000,
   fetch(req, server) {
     const url = new URL(req.url);
     if (url.pathname === "/chat") {
@@ -10,10 +12,16 @@ const server = Bun.serve<{ userId: string; channelId: string }>({
         const roomIdQuery = req.url.split("?")[1];
         const roomId = req.url.split("=")[1];
         console.log("room", roomId);
-        const token = req.headers.get("Authorization");
-        const tokenString = token?.split(" ")[1];
-        const decoded = jwt.verify(tokenString, secret);
-        // console.log("userId", decoded.sub);
+        // const token = req.headers.get("Authorization");
+        // const token = req.headers.get("authjs.session-token");
+        const cookieFromHeaders = req.headers.get("Cookie");
+        var parsedCookie = cookie.parse(cookieFromHeaders);
+        // console.log("cookie", parsedCookie);
+        const token = parsedCookie["Authorization"];
+        console.log("token", token);
+        // console.log("token", typeof token);
+        const decoded = jwt.verify(token, secret);
+        console.log("userId", decoded);
         const userId = decoded.sub;
         const success = server.upgrade(req, {
           data: { userId: userId, channelId: roomId },
@@ -32,14 +40,40 @@ const server = Bun.serve<{ userId: string; channelId: string }>({
   },
   websocket: {
     open(ws) {
-      const msg = `${ws.data.userId} has entered the chat in channel ${ws.data.channelId}`;
+      // WHEN USER JOINS THE CHANNEL ROOM FOR THE FIRST TIME
+      // this gets triggered/run
+
+      // const msg = `${ws.data.userId} has entered the chat in channel ${ws.data.channelId}`;
+      const msg = {
+        event: "user-join",
+        userId: ws.data.userId,
+        channelId: ws.data.channelId,
+      };
+
+      console.log(ws.data);
       ws.subscribe(ws.data.channelId);
-      server.publish(ws.data.channelId, msg);
+      server.publish(ws.data.channelId, JSON.stringify(msg));
     },
     message(ws, message) {
+      // HERE WE ARE RECEIVING THE MESSAGE FROM THE CLIENT(NEXT.JS AUCTION BIDDING AND CHAT ROOM)
+      // AS THE MESSAGE IS RECEIVED IS IN STRING || BUFFERS, WE PARSE IT TO STRING AND THEN JSON
+      const parsedMessage = JSON.parse(String(message));
+      console.log("wsData", ws);
+      console.log("message", parsedMessage);
+      console.log("accessObj", parsedMessage?.event);
+      if (parsedMessage?.event == "chat-message") {
+        server.publish(ws.data.channelId, JSON.stringify(parsedMessage));
+      }
+      // NOW WHEN THE DATA IS CONFIRMED TO BE A JSON
+
+      // MANAGE YOUR EVENTS HERE
+
+      // 1. receive chat message event
+
       // this is a group chat
       // so the server re-broadcasts incoming message to everyone
-      server.publish(ws.data.channelId, `${ws.data.userId}: ${message}`);
+
+      // 2. receive auction bid where user send me productid and bid price
     },
     close(ws) {
       const msg = `${ws.data.userId} has left the chat in channel ${ws.data.channelId}`;
